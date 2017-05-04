1da7de7ef93b05459c61a81477b516b8bf0e0fa1
Solomon Tang <solomoncztang@gmail.com>
2017-05-04 02:18:22 -0700

    Removed unnecessary conditional for unstringifyible values.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -10,8 +10,6 @@ var stringifyJSON = function(obj) {
     return stringifyObj(obj);
   } else if (typeof obj === 'string') {
     return stringifyStr(obj);
-  } else if (typeof obj === 'function' || obj === undefined) {
-    return '';
   }
   return String(obj);
 }
@@ -20,25 +18,29 @@ var stringifyArr = function(arr) {
   var stringify = arr.reduce(function(string, ele) {
     return string + stringifyJSON(ele) + ',';
   },'[')
-  if (stringify.length !== 1) {
+  //strip last character (comma) if not default;
+  if (stringify !== '[') {
     stringify = stringify.slice(0,-1);
   }
+
   return stringify + ']';
 }
 
-var stringifyStr = function(obj) {
-  return '"' + obj + '"';
+var stringifyStr = function(str) {
+  return '"' + str + '"';
 }
 
 var stringifyObj = function(obj) {
   var stringify = Object.keys(obj).reduce(function(string, ele) {
-    if (typeof obj[ele] === 'function' || obj[ele] === undefined) {
-      return string + '';
+    if (typeof obj[ele] === 'function' || obj[ele] === undefined) { //handle unstringifiableValues
+      return string;
     }
     return string + '"' + String(ele) + '"' + ':' + stringifyJSON(obj[ele]) + ',';
   },"{");
-  if( stringify.length !== 1) {
+  //strip last character (comma) if not default;
+  if( stringify !== "{") {
     stringify = stringify.slice(0, -1);
   }
+
   return stringify + '}';
 }
