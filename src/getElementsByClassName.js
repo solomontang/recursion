3b2900b8605815177143d976150fe4ec731cc7e0
Solomon Tang <solomoncztang@gmail.com>
2017-05-07 17:38:25 -0700

    Completed support for getElementsByClassName.js

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,21 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className){
+  var results= [];
+  var body = document.body;
+
+  var findNode = function(node)  {
+    if(node.classList && node.classList.contains(className))  {
+      results.push(node);
+    }
+    if(node.childNodes) {
+      node.childNodes.forEach(function(child) {
+        findNode(child);
+      });
+    }
+  };
+
+  findNode(body);
+  return results;
 };
